7e58221c23272f4182929289a3713ae9e243d0d1
System Administrator <110475574+Pablineee@users.noreply.github.com>
2025-02-05 13:08:02 -0500

    config/db.js updated, socket.io implemented within server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const socketIo = require('socket.io');
+const socket = require('socket.io');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -11,12 +11,57 @@ const SERVER_PORT = process.env.PORT || 8000;
 // Initiate MongoDB connection
 connectDB();
 
+// Start server
 const server = app.listen(SERVER_PORT, () => {
     console.log(`Chat Server running on http://localhost:${SERVER_PORT}`);
 });
 
-const io = socketIo(server);
+const io = socket(server);
 
 // Import routes
 const authRoutes = require('./routes/auth');
 const chatRoutes = require('./routes/chat');
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// API Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/chat', chatRoutes);
+
+// Socket.io implementation
+io.on('connection', (socket) => {
+    console.log(`New Socket: ${socket.id}`);
+
+    socket.on('disconnect', () => {
+        console.log(`User ${socket.id} disconnected`);
+    });
+
+    socket.on('message', (data) => {
+        console.log(`Message from ${socket.id}: ${data}`);
+    });
+
+    socket.on('chat_message', (data) => {
+        data.clientId = socket.id;
+        console.log(JSON.stringify(data));
+        //socket.emit('chat_message', data)
+        io.emit('chat_message', data)
+        //socket.broadcast.emit('chat_message', data)
+    });
+
+    socket.on('join_group', (roomName) => {
+        console.log(`User ${socket.id} joined room ${roomName}`);
+        socket.join(roomName);
+    });
+
+    socket.on('leave_group', (roomName) => {
+        socket.leave(roomName);
+    });
+
+    socket.on('group_message', (data) => {
+        console.log(`User ${socket.id} sent a message to room ${data.group}`);
+        data.senderId = socket.id;
+        io.to(data.group).emit('group_message', data);
+    });
+});
