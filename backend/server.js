12f0abb76acf98e8dfcbbc30346e32572600e8fe
System Administrator <110475574+Pablineee@users.noreply.github.com>
2025-02-05 12:33:14 -0500

    Node.JS project initialized, project structure defined and server.js development commenced

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const socketIo = require('socket.io');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const SERVER_PORT = process.env.PORT || 8000;
+
+const server = app.listen(SERVER_PORT, () => {
+    console.log(`Chat Server running on http://localhost:${SERVER_PORT}`);
+});
+
+const io = socketIo(server);
+
+// Import routes
+const authRoutes = require('./routes/auth');
+const chatRoutes = require('./routes/chat');
